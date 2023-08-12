ce05e0f5cc0322f77e9ae9510cf776829b49ec3c
tascheidt <88148089+tascheidt@users.noreply.github.com>
2023-08-11 20:21:51 -0500

    adding apikeys file

---
+++ apikeys.py
@@ -0,0 +1,7 @@
+# OpenAI API Key; we use GPT-4 in this demo
+# Google API Key for using its web search components + a search ID
+# Our search ID has a global web context; i.e., we simply use the base Google search offering
+
+openai_key = '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'
+google_api_key = 'AIzaSyDAJEjbQRwZnC1L3O7PoddpJEedr-jG0_o'
+search_id = 'f5da9b45ac43349df'
