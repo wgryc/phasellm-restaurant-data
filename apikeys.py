d67f8faf562e14f6ac1ab3a99a662b1c578c9b80
tascheidt <88148089+tascheidt@users.noreply.github.com>
2023-08-12 07:35:34 -0500

    Placeholders for API keys

---
+++ apikeys.py
@@ -0,0 +1,8 @@
+#Store API keys here to protect them from being exposed in the notebook
+# OpenAI API Key; we use GPT-4 in this demo
+# Google API Key for using its web search components + a search ID
+# Our search ID has a global web context; i.e., we simply use the base Google search offering
+
+openai_key = "add openai key here"
+google_api_key = "add google api key here"
+search_id = "add google search id here"
